b108a6061b6741c4ba51e8d775e39d6eddba93fe
HusneShabbir <husneshabbir447@gmail.com>
2025-10-18 16:57:51 +0530

    skip bulk import tests

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  /* Opt out of parallel tests on CI. */ 
   workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
